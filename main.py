2760264177af4f13259eec116a692a79e763affa
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-03-02 16:04:50 +0600

    maybe fixed

--- main.py
+++ main.py
@@ -154,4 +154,8 @@ if __name__ == "__main__":
     vse = VidSrcExtractor(source_name =SUPPORTED_SOURCES[0] ,
         fetch_subtitles = True,)
     
-    app.run()
+    
+    host = os.environ.get('FLASK_RUN_HOST', '0.0.0.0')
+    port = int(os.environ.get('FLASK_RUN_PORT', 5000))
+
+    app.run(host=host, port=port, debug=False)

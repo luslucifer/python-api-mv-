f2b7db42069ec733f021ac5b15f4beb464fb5309
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-03-16 23:28:48 +0600

    fixed

--- utils.py
+++ utils.py
@@ -1,90 +1,55 @@
 import base64
-import subprocess
-from typing import Union
 
-# Helper methods
 class Utilities:
     @staticmethod
-    def check_mpv_exists() -> bool:
-        try:
-            subprocess.run(['mpv', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-            return True
-        except subprocess.CalledProcessError:
-            return False
-
-    @staticmethod
-    def decode_data(key: str, data: Union[bytearray, str]) -> bytearray:
-        key_bytes = bytes(key, 'utf-8')
-        s = bytearray(range(256))
-        j = 0
-
-        for i in range(256):
-            j = (j + s[i] + key_bytes[i % len(key_bytes)]) & 0xff
-            s[i], s[j] = s[j], s[i]
-
-        decoded = bytearray(len(data))
-        i = 0
-        k = 0
-
-        for index in range(len(data)):
-            i = (i + 1) & 0xff
-            k = (k + s[i]) & 0xff
-            s[i], s[k] = s[k], s[i]
-            t = (s[i] + s[k]) & 0xff
-
-            if isinstance(data[index], str):
-                decoded[index] = ord(data[index]) ^ s[t]
-            elif isinstance(data[index], int):
-                decoded[index] = data[index] ^ s[t]
-            else:
-                raise RC4DecodeError("Unsupported data type in the input")
-
+    def decode_src(encoded, seed) -> str:
+        encoded_buffer = bytes.fromhex(encoded)
+        decoded = ""
+        for i in range(len(encoded_buffer)):
+            decoded += chr(encoded_buffer[i] ^ ord(seed[i % len(seed)]))
         return decoded
-    
+
     @staticmethod
-    def int_2_base(x: int, base: int) -> str:
-        charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/"
+    def hunter(h, u, n, t, e, r) -> str:
+        def hunter_def(d, e, f) -> int:
+            charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/"
+            source_base = charset[0:e]
+            target_base = charset[0:f]
 
-        if x < 0:
-            sign = -1
-        elif x == 0:
-            return 0
-        else:
-            sign = 1
+            reversed_input = list(d)[::-1]
+            result = 0
 
-        x *= sign
-        digits = []
+            for power, digit in enumerate(reversed_input):
+                if digit in source_base:
+                    result += source_base.index(digit) * e**power
 
-        while x:
-            digits.append(charset[int(x % base)])
-            x = int(x / base)
+            converted_result = ""
+            while result > 0:
+                converted_result = target_base[result % f] + converted_result
+                result = (result - (result % f)) // f
+
+            return int(converted_result) or 0
         
-        if sign < 0:
-            digits.append('-')
-        digits.reverse()
+        i = 0
+        result_str = ""
+        while i < len(h):
+            j = 0
+            s = ""
+            while h[i] != n[e]:
+                s += h[i]
+                i += 1
 
-        return ''.join(digits)
-    
+            while j < len(n):
+                s = s.replace(n[j], str(j))
+                j += 1
+
+            result_str += chr(hunter_def(s, e, 10) - t)
+            i += 1
+
+        return result_str
+
     @staticmethod
     def decode_base64_url_safe(s: str) -> bytearray:
         standardized_input = s.replace('_', '/').replace('-', '+')
         binary_data = base64.b64decode(standardized_input)
         return bytearray(binary_data)
-    
-    
-# Errors
-class VidSrcError(Exception):
-    '''Base Error'''
-    pass
-
-class CouldntFetchKeys(VidSrcError):
-    '''Failed to fetch decryption keys for vidplay'''
-    pass
-
-class RC4DecodeError(VidSrcError):
-    '''Failed to decode RC4 data (current design choices == only ever ValueError)'''
-    pass
-
-class NoSourcesFound(VidSrcError):
-    '''Failed to find any media sources @ the provided source'''
-    pass
---
+++ vidsrc.py
@@ -0,0 +1,111 @@
+import os
+import requests
+
+from utils import Utilities
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+from typing import Optional, Tuple, Dict
+
+from sources.superembed import MultiembedExtractor
+from sources.vidsrcpro import VidsrcStreamExtractor
+
+SUPPORTED_SOURCES = ["VidSrc PRO", "Superembed"]
+
+class VidsrcMeExtractor:
+    BASE_URL : str = "https://vidsrc.me" # vidsrc.me / vidsrc.in / vidsrc.pm / vidsrc.xyz / vidsrc.net
+    RCP_URL : str = "https://rcp.vidsrc.me/rcp"
+
+    def __init__(self, **kwargs) -> None:
+        self.source_name = kwargs.get("source_name")
+        self.fetch_subtitles = kwargs.get("fetch_subtitles")
+
+    def get_sources(self, url: str) -> Tuple[Dict, str]:
+        print(f"[>] Requesting {url}...")
+        req = requests.get(url)
+
+        if req.status_code != 200:
+            print(f"[VidSrcExtractor] Couldnt fetch \"{req.url}\", status code: {req.status_code}\n[VidSrcExtractor] \"{self.BASE_URL}\" likely doesnt have the requested media...")
+            return {}, f"https://{urlparse(req.url).hostname}/"
+        
+        soup = BeautifulSoup(req.text, "html.parser")
+        return {source.text: source.get("data-hash") for source in soup.find_all("div", {"class", "server"}) if source.text and source.get("data-hash")}, f"https://{urlparse(req.url).hostname}/"
+    
+    def get_source(self, hash: str, referrer: str) -> Tuple[Optional[str], str]:
+        url = f"{self.RCP_URL}/{hash}"
+        print("[>] Requesting RCP domain...")
+
+        req = requests.get(url, headers={"Referer": referrer})
+        if req.status_code != 200:
+            print(f"[VidSrcExtractor] Couldnt fetch \"{url}\", status code: {req.status_code}")
+            return None, url
+
+        soup = BeautifulSoup(req.text, "html.parser")
+        encoded = soup.find("div", {"id": "hidden"}).get("data-h")
+        seed = soup.find("body").get("data-i") # this is just the imdb id - the "tt" lol
+
+        source = Utilities.decode_src(encoded, seed)
+        if source.startswith("//"):
+            source = f"https:{source}"
+
+        return source, url
+    
+    def get_source_url(self, url: str, referrer: str) -> Optional[str]:
+        print("[>] Requesting source url...")
+        req = requests.get(url, allow_redirects=False, headers={"Referer": referrer})
+        if req.status_code != 302:
+            print(f"[VidSrcExtractor] Couldnt find redirect for \"{url}\", status code: {req.status_code}")
+            return None
+        
+        return req.headers.get("location")
+
+    def get_streams(self, media_id: str, season: Optional[str], episode: Optional[str]) -> Optional[Dict]:
+        url = f"{self.BASE_URL}/embed/{media_id}"
+        if season and episode:
+            url += f"/{season}-{episode}/"
+
+        sources, sources_referrer = self.get_sources(url)
+        source = sources.get(self.source_name)
+        if not source:
+            available_sources = ", ".join(list(sources.keys()))
+            print(f"[VidSrcExtractor] No source found for \"{self.source_name}\"\nAvailable Sources: {available_sources}")
+            return None
+
+        source_url, source_url_referrer = self.get_source(source, sources_referrer)
+        if not source_url:
+            print(f"[VidSrcExtractor] Could not retrieve source url, please check you can request \"{url}\", if this issue persists please open an issue.")
+            return None
+        
+        final_source_url = self.get_source_url(source_url, source_url_referrer)
+        if "vidsrc.stream" in final_source_url:
+            print(f"[>] Fetching source for \"{self.source_name}\"...")
+
+            extractor = VidsrcStreamExtractor()
+            return extractor.resolve_source(url=source_url, referrer=source_url_referrer)
+        
+        elif "multiembed.mov" in final_source_url:
+            extractor = MultiembedExtractor()
+            return extractor.resolve_source(url=source_url, referrer=source_url_referrer)
+        
+        return None
+
+if __name__ == "__main__":
+    # default_language = "English"
+    # print("Supported sources -")
+    # for i, v in enumerate(SUPPORTED_SOURCES, start=1):
+    #     print(f"{i} - {v}")
+
+    # source_index = (int(input("Select Source: ")) - 1) % len(SUPPORTED_SOURCES)
+    # media_id = input("Input imdb/tmdb code: ")
+
+    vse = VidsrcMeExtractor(
+        source_name=SUPPORTED_SOURCES[0],
+        fetch_subtitles=True
+    )
+
+    stream_data = vse.get_streams(
+        media_id=123,
+        season=None,
+        episode=None
+    )
+
+    # print(stream_data)
